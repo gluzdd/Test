80648146ffe77321e9f1210d9b7c26def77f972e
Artem <Artem@Nesterov.ru>
2023-06-28 21:32:17 +0300

    Программа, решающая поставленную задачу

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] array = new string[4] {"Hello", "2", "world", ":-)"};
+string[] secondArray = new string[array.Length];
+
+void GetShortWordsArray(string[] array, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            secondArray[count] = array[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+GetShortWordsArray(array, secondArray);
+PrintArray(secondArray);
